9345be16c8555cb306ea1640a8aeed6bbf6664ca
Brexton Lane <19.brexton.lane@ksd.org>
2019-02-06 07:26:04 -0800

    ui and joystick code

---
+++ Mobile game android ios/Assets/Scripts/Controller.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Transform player;
+    public float Speed = 5.0f;
+    private bool TouchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
+
+    public Transform Circle;
+    public Transform OuterCircle;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+
+            Circle.transform.position = pointA * -1;
+            OuterCircle.transform.position = pointA * -1;
+            Circle.GetComponent<SpriteRenderer>().enabled = true;
+            OuterCircle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            TouchStart = true;
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+        else
+        {
+            TouchStart = false;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if(TouchStart)
+        {
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharacter(direction * -1);
+
+            Circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y) * -1;
+        }
+        else
+        {
+            Circle.GetComponent<SpriteRenderer>().enabled = false;
+            OuterCircle.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        player.Translate(direction * Speed * Time.deltaTime);
+    }
+}
--- Mobile game android ios/Assets/Scripts/UI.cs
+++ Mobile game android ios/Assets/Scripts/UI.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    public int Health = 10;
+    public Text healthText;
+    public Slider healthBar;
 
-	// Use this for initialization
-	void Start ()
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        healthText.GetComponent<Text>().text = "Health: " + Health;
+        healthBar.GetComponent<Slider>().value = Health;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-		
-	}
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Health--;
+            healthText.GetComponent<Text>().text = "Health: " + Health;
+            healthBar.GetComponent<Slider>().value = Health;
+            if (Health <= 0)
+
+            {
+                //Reload the level
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
 }
+
