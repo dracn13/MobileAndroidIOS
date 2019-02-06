e0a527a7642d981bf235bde3417c956f975e54cc
Alejandro Magana <20.alejandro.magana@ksd.org>
2019-02-06 08:46:55 -0800

    adding keyboard movement and player animator controller

---
+++ Mobile game android ios/Assets/Scripts/PlayerAnimationControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Horizontal") != 0)
+        {
+            float x = Input.GetAxis("Horizontal");
+            GetComponent<Animator>().SetFloat("x", x);
+            GetComponent<Animator>().SetFloat("y", 0);
+            GetComponent<Animator>().SetBool("Idle", false);
+        }else if (Input.GetAxis("Vertical") != 0)
+        {
+            float y = Input.GetAxis("Vertical");
+            GetComponent<Animator>().SetFloat("y", y);
+            GetComponent<Animator>().SetFloat("x", 0);
+            GetComponent<Animator>().SetBool("Idle", false);
+        }else
+        {
+            GetComponent<Animator>().SetBool("Idle", true);
+            GetComponent<Animator>().SetFloat("y", 0);
+            GetComponent<Animator>().SetFloat("x", 0);
+        }
+    }
+}
