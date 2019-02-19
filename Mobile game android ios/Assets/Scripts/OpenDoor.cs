aba657de5f1cb54f258c12af19ebae6a05502d50
Alejandro Magana <20.alejandro.magana@ksd.org>
2019-02-19 10:19:08 -0800

    adding doors and level 2 tilemap

---
+++ Mobile game android ios/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject Door;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Door.SetActive(false);
+        Destroy(gameObject);
+    }
+}
