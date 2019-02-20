69e5001c8039a26c1b58becaaecd4a6c2c81ce97
Brexton Lane <19.brexton.lane@ksd.org>
2019-02-20 09:59:18 -0800

    just pushing out so i can fix the other levels UI

--- Mobile game android ios/Assets/Scripts/MenuButtons.cs
+++ Mobile game android ios/Assets/Scripts/MenuButtons.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+// to do:
+//
+// make so you can`t open pause menu in difficulty settings
+//----opening and closing pause menu in difficulty settings sets timescale to 1 allowing movement
+//
+//
 public class MenuButtons : MonoBehaviour
 {
 
     public GameObject PauseMenu;
+    public GameObject UI;
     public GameObject DifficultySettings;
     // pause button
     public void Pause()
@@ -15,6 +22,7 @@ public class MenuButtons : MonoBehaviour
             Time.timeScale = 0;
             //make pause menu visible
             PauseMenu.GetComponent<Canvas>().enabled = true;
+            UI.GetComponent<Canvas>().enabled = false;
         }
         else if (Time.timeScale == 0)
         {
@@ -36,7 +44,9 @@ public class MenuButtons : MonoBehaviour
     // resume button
     public void Resume()
     {
+        UI.GetComponent<Canvas>().enabled = true;
         PauseMenu.GetComponent<Canvas>().enabled = false;
+        DifficultySettings.GetComponent<Canvas>().enabled = false;
         Time.timeScale = 1;
     }
     //retry button
@@ -51,6 +61,8 @@ public class MenuButtons : MonoBehaviour
     {
         DifficultySettings.GetComponent<Canvas>().enabled = true;
         PauseMenu.GetComponent<Canvas>().enabled = false;
+        UI.GetComponent<Canvas>().enabled = false;
+
     }
     //for the setting a go back button
     public void GoBack()
