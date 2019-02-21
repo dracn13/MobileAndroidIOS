ca37ee126bb1bdf70e868a9dfe52c387877a1446
Brexton Lane <19.brexton.lane@ksd.org>
2019-02-21 09:32:37 -0800

    updating the health script

--- Mobile game android ios/Assets/Scripts/UI.cs
+++ Mobile game android ios/Assets/Scripts/UI.cs
@@ -9,27 +9,51 @@ public class UI : MonoBehaviour
     public int Health = 10;
     public Text healthText;
     public Slider healthBar;
+    float timer = 0;
 
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
     void Start()
     {
         healthText.GetComponent<Text>().text = "Health: " + Health;
         healthBar.GetComponent<Slider>().value = Health;
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (timer > 0.5f && collision.gameObject.tag == "Enemy" || timer > 0.5f && collision.gameObject.tag == "Boss")
         {
+            timer = 0;
             Health--;
             healthText.GetComponent<Text>().text = "Health: " + Health;
             healthBar.GetComponent<Slider>().value = Health;
             if (Health <= 0)
 
             {
-                //Reload the level
+                Time.timeScale = 0;
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
     }
+
 }
 
+
+
+//void OnCollisionEnter2D(Collision2D collision)
+//{
+   // if (collision.gameObject.tag == "Enemy")
+   // {
+      //  Health--;
+       // healthText.GetComponent<Text>().text = "Health: " + Health;
+       // healthBar.GetComponent<Slider>().value = Health;
+       // if (Health <= 0)
+
+       // {
+            //Reload the level
+       //     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+       // }
+   // }
+//}
