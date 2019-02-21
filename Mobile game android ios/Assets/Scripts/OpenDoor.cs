58d6609d8e1120025ad947f4adc5f7519d9ad2a4
Alejandro Magana <20.alejandro.magana@ksd.org>
2019-02-21 10:27:26 -0800

    changing the way the keys script

--- Mobile game android ios/Assets/Scripts/OpenDoor.cs
+++ Mobile game android ios/Assets/Scripts/OpenDoor.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     public GameObject Door;
+    public bool YouHaveKey;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Door.SetActive(false);
+        //Door.SetActive(false);
+        Debug.Log("You have a key");
         Destroy(gameObject);
     }
 }
