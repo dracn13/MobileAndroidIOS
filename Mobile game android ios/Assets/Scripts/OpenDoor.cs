ad9ac5e2903ebd1f747249c3609b36564aceb48a
Colby Johnson <20.colby.johnson@ksd.org>
2019-03-04 08:17:00 -0800

    UI

--- Mobile game android ios/Assets/Scripts/OpenDoor.cs
+++ Mobile game android ios/Assets/Scripts/OpenDoor.cs
@@ -1,31 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenDoor : MonoBehaviour
 {
  
     public int RedKeyCount = 0;
     public int BlueKeyCount = 0;
+    public RawImage RedKey;
+    public RawImage BlueKey;
 
      void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Bluekey")
         {
-            
+            BlueKey.GetComponent<RawImage>().enabled = true;
             BlueKeyCount++;
             Destroy(collision.gameObject);
         }
         if (collision.gameObject.tag == "Redkey")
         {
-            
+            RedKey.GetComponent<RawImage>().enabled = true;
             RedKeyCount++;
             Destroy(collision.gameObject);
+            
         }
         if (BlueKeyCount >= 1)
         {
             if (collision.gameObject.tag == "Bluedoor")
             {
+                BlueKey.GetComponent<RawImage>().enabled = false;
                 Destroy(collision.gameObject);
                 BlueKeyCount--;
             }
@@ -34,6 +39,7 @@ public class OpenDoor : MonoBehaviour
         {
             if (collision.gameObject.tag == "Reddoor")
             {
+                RedKey.GetComponent<RawImage>().enabled = false;
                 Destroy(collision.gameObject);
                 RedKeyCount--;
             }
