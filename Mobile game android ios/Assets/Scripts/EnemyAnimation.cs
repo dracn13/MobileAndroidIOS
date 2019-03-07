b0b07412bffb5a9a58a1693f9785c735649623bd
Colby Johnson <20.colby.johnson@ksd.org>
2019-03-07 09:46:42 -0800

    Boss Enemy

--- Mobile game android ios/Assets/Scripts/EnemyAnimation.cs
+++ Mobile game android ios/Assets/Scripts/EnemyAnimation.cs
@@ -4,51 +4,48 @@ using UnityEngine;
 
 public class EnemyAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D Child;
+    public Animator ChildAmine;
 
     // Update is called once per frame
     void Update()
     {
-        float x = GetComponent<Rigidbody2D>().velocity.x;
-        float y = GetComponent<Rigidbody2D>().velocity.y;
+        float x = Child.velocity.x;
+        float y = Child.velocity.y;
         
         if(x < 0)
         {
-            GetComponent<Animator>().SetFloat("x", x);
-            GetComponent<Animator>().SetFloat("y", 0);
-            GetComponent<Animator>().SetBool("idle", false);
+            ChildAmine.SetFloat("x", x);
+            ChildAmine.SetFloat("y", 0);
+            ChildAmine.SetBool("idle", false);
         }
 
         if (x > 0)
         {
-            GetComponent<Animator>().SetFloat("x", x);
-            GetComponent<Animator>().SetFloat("y", 0);
-            GetComponent<Animator>().SetBool("idle", false);
+            ChildAmine.SetFloat("x", x);
+            ChildAmine.SetFloat("y", 0);
+            ChildAmine.SetBool("idle", false);
         }
 
         if (y > 0)
         {
-            GetComponent<Animator>().SetFloat("y", y);
-            GetComponent<Animator>().SetFloat("x", 0);
-            GetComponent<Animator>().SetBool("idle", false);
+            ChildAmine.SetFloat("y", y);
+            ChildAmine.SetFloat("x", 0);
+            ChildAmine.SetBool("idle", false);
         }
 
         if (y < 0)
         {
-            GetComponent<Animator>().SetFloat("y", y);
-            GetComponent<Animator>().SetFloat("x", 0);
-            GetComponent<Animator>().SetBool("idle", false);
+            ChildAmine.SetFloat("y", y);
+            ChildAmine.SetFloat("x", 0);
+            ChildAmine.SetBool("idle", false);
         }
 
         if (x == 0 && y == 0)
         {
-            GetComponent<Animator>().SetBool("idle", true);
-            GetComponent<Animator>().SetFloat("y", 0);
-            GetComponent<Animator>().SetFloat("x", 0);
+            ChildAmine.SetBool("idle", true);
+            ChildAmine.SetFloat("y", 0);
+            ChildAmine.SetFloat("x", 0);
         }
     }
 }
---
+++ Mobile game android ios/Assets/Scripts/EnemyChild.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChild : MonoBehaviour
+{
+    public GameObject Mother;
+    public float chaseSpeed = 10.0f;
+    public float chaseTriggerDistance = 10.0f;
+    Vector3 startPosition;
+    public Vector3 paceDirection = new Vector3(0, 0, 0);
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mother = GameObject.FindGameObjectWithTag("Boss");
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 playerPosition = Mother.transform.position;
+        //vector form enemy position to the players position
+        Vector3 chaseDirection = playerPosition - transform.position;
+        if (chaseDirection.magnitude < chaseTriggerDistance)
+        {
+            //chase because the palyer is close to the enemy
+            chaseDirection.Normalize();
+            GetComponent<Rigidbody2D>().velocity = chaseDirection * chaseSpeed;
+        }
+
+
+    }
+}
