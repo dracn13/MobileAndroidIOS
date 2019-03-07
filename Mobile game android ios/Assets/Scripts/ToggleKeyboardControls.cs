92eaf90725cc76f8a6bb543a09cc34190c27dd46
Alejandro Magana <20.alejandro.magana@ksd.org>
2019-03-07 09:33:24 -0800

    Added buttons to toggle keyboard or touch controls

---
+++ Mobile game android ios/Assets/Scripts/ToggleKeyboardControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleKeyboardControls : MonoBehaviour
+{
+    public GameObject TOMMY;
+    
+    public void Keyboard()
+    {
+        TOMMY.GetComponent<Movement>().enabled = true;
+        TOMMY.GetComponent<PlayerAnimationControl>().enabled = true;
+       
+    }
+    
+    public void TouchControls()
+    {
+        TOMMY.GetComponent<Movement>().enabled = false;
+        TOMMY.GetComponent<PlayerAnimationControl>().enabled = false;
+    }
+    
+}
