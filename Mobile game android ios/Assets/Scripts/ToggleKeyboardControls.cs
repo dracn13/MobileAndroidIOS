c91f821193102602cdbc5453d2beffdcd85f125b
Alejandro Magana <20.alejandro.magana@ksd.org>
2019-03-07 10:21:56 -0800

    added visual for choosing controls

--- Mobile game android ios/Assets/Scripts/ToggleKeyboardControls.cs
+++ Mobile game android ios/Assets/Scripts/ToggleKeyboardControls.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToggleKeyboardControls : MonoBehaviour
 {
     public GameObject TOMMY;
-    
+    public RawImage OnKeyboard;
+    public RawImage OnTouch;
+
     public void Keyboard()
     {
         TOMMY.GetComponent<Movement>().enabled = true;
         TOMMY.GetComponent<PlayerAnimationControl>().enabled = true;
-       
+        OnKeyboard.GetComponent<RawImage>().enabled = true;
+        OnTouch.GetComponent<RawImage>().enabled = false;
+
     }
     
     public void TouchControls()
     {
         TOMMY.GetComponent<Movement>().enabled = false;
         TOMMY.GetComponent<PlayerAnimationControl>().enabled = false;
+        OnKeyboard.GetComponent<RawImage>().enabled = false;
+        OnTouch.GetComponent<RawImage>().enabled = true;
+
     }
     
 }
